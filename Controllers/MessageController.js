f5fb4ac88d26db12b852f0886acfc922e553cdd1
Ishwar Datt Mishra <ishwardatt.mishra@gmail.com>
2021-03-08 18:40:08 +0530

    Completd api

--- Controllers/MessageController.js
+++ Controllers/MessageController.js
@@ -6,15 +6,23 @@ const { v4: uuidv4 } = require('uuid');
 
 
 
-module.exports.getNote = (req, res) => {
-    var ciphertext = "U2FsdGVkX1+/cjPWHuct6K3CltJamuIO5ZvYoq8qzZ+azvy39g2KTMCqnJU0";
+module.exports.getNote = async (req, res) => {
+    let messageId = req.params.messageId;
+    let key = req.body.key;
+    let note;
     try {
-        var bytes = cryptoRabbit.decrypt(ciphertext, req.body.key);
+        note = await Notes.findOne({ url: messageId });
+        var bytes = cryptoRabbit.decrypt(note.message, key);
         var originalText = bytes.toString(CryptoJS.enc.Utf8);
+        note.delete();
         res.send({ message: originalText })
-    }
-    catch (e) {
-        res.send({ message: "Opss! They key is not correct note will be deleted after 3 attempts" })
+    } catch (e) {
+        note.attempts--;
+        if (note.attempts === 0) {
+            note.delete();
+            res.send({ status: 'error' , message: "Opps!! you have used three attempts message has been deleted" })
+        }
+        res.send({ status: 'error', message: "Opps!! Key seems to be wrong , message will be deletedafter 3 attempts." })
     }
 }
 
@@ -27,9 +35,9 @@ module.exports.saveNote = async (req, res) => {
     notes.attempts = 3;
     try {
         await notes.save();
-        let fullUrl = req.protocol + '://' + req.get('host') +'/get-note/'+ uuid;
-        res.send({ message: "Saved",url :fullUrl })
+        let fullUrl = req.protocol + '://' + req.get('host') + '/get-note/' + uuid;
+        res.send({ status: 'success' ,message: "Saved", url: fullUrl })
     } catch (e) {
-        res.send({ message: "Something went wrong please try again!" })
+        res.send({ status: 'error', message: "Something went wrong please try again!" })
     }
 }
--- Routes/routes.js
+++ Routes/routes.js
@@ -3,7 +3,7 @@ const messageController = require('../Controllers/MessageController')
 const router = express.Router();
 
 router.post('/save-note', messageController.saveNote);
-router.post('/get-note', messageController.getNote);
+router.post('/get-note/:messageId', messageController.getNote);
 router.get('/', (req, res) => {
     res.send("Welcome To Secure Note")
 })
